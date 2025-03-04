09ea0952db88693779777733016169bd9712c00c
Mehul <mehtomar@gmail.com>
2025-03-04 17:54:50 -0500

    Testing to move the camera of the robot around

--- src/keyboardMove/main.py
+++ src/keyboardMove/main.py
@@ -3,6 +3,9 @@ import termios
 import time
 import sys
 import os
+
+import hiwonder.Board as Board
+import hiwonder.Camera as Camera
 import hiwonder.ActionGroupControl as AGC
 
 instructions = '''Controls:\r
@@ -11,6 +14,10 @@ a: Turn Left\r
 s: Move Backward\r
 d: Turn Right\r
 q: Quit\r'''
+
+def initCamera():
+    Board.setPWMServoPulse(1, 1500, 500)
+    Board.setPWMServoPulse(2, 1500, 500)
 
 def enterRaw():
     fd = sys.stdin.fileno()
@@ -26,6 +33,9 @@ def main():
     print(instructions)
     ch = ''
     os.set_blocking(sys.stdin.fileno(), False)
+    initCamera()
+    yCoord = 1500
+    xCoord = 1500
     while ch != 'q':
         try:
             ch = chr(os.read(sys.stdin.fileno(), 1024)[-1])
@@ -43,6 +53,26 @@ def main():
                 AGC.runActionGroup('turn_right')
             elif ch == 'q':
                 print('Quitting...\r')
+            elif ch == 'h':
+                print('Move Camera Left')
+                xCoord -= 20
+                xCoord = 500 if xCoord < 500 else xCoord
+                Board.setPWMServoPulse(2, xCoord, 0.001)
+            elif ch == 'j':
+                print('Move Camera Down')
+                yCoord -= 20
+                yCoord = 1000 if yCoord < 1000 else yCoord
+                Board.setPWMServoPulse(1, yCoord, 0.001)
+            elif ch == 'k':
+                print('Move Camera Up')
+                yCoord += 20
+                yCoord = 2000 if yCoord > 2000 else yCoord
+                Board.setPWMServoPulse(1, yCoord, 0.001)
+            elif ch == 'l':
+                print('Move Camera Right')
+                xCoord += 20
+                xCoord = 2500 if xCoord < 2500 else xCoord
+                Board.setPWMServoPulse(2, xCoord, 0.001)
         except:
             continue
     exitRaw(fd, oSet)
